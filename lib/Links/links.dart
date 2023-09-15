1dc5a34f60ee93137fa43a3c8c4f50c1e6de07a1
dishankunnummal <bahiznalakath123@gmail.com>
2023-09-15 22:03:55 +0530

    todo addedsomw changesin code

--- lib/Links/links.dart
+++ lib/Links/links.dart
@@ -21,8 +21,7 @@ const String buymeacoffee = 'https://www.buymeacoffee.com/muhammedbahizn';
 const String profileImage =
     "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
 //
-// Github Link
-const String fiverr = 'https://www.fiverr.com/bahiznalakath';
+
 // Github Link
 const String upwork = 'https://www.upwork.com/freelancers/bahiznalakath';
 // Github Link
